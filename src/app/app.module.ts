08c8c3acd768a62cccde91128716f76edba5aa45
Michal Hromas <michalhromas1@gmail.com>
2022-03-25 22:29:32 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, FormsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, GridModule],
   providers: [],
   bootstrap: [AppComponent],
 })
