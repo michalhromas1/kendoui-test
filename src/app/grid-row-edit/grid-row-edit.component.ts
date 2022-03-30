dccc43647efee7d57dd3b2cc69ef818967cca800
Michal Hromas <michalhromas1@gmail.com>
2022-03-30 16:07:28 +0200

    edit-row

--- src/app/grid-row-edit/grid-row-edit.component.ts
+++ src/app/grid-row-edit/grid-row-edit.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { GridItem } from '@progress/kendo-angular-grid';
 import { getProducts, Product } from '../mocks';
 
@@ -10,6 +10,9 @@ import { getProducts, Product } from '../mocks';
 })
 export class GridRowEditComponent {
   products = getProducts().slice(0, 10);
+  formGroup: FormGroup | undefined;
+
+  private editedRowIndex: number | undefined;
 
   constructor(private formBuilder: FormBuilder) {}
 
@@ -29,5 +32,42 @@ export class GridRowEditComponent {
   trackBy(_index: number, item: GridItem): number {
     const product = item.data as Product;
     return product.ProductID;
+  }
+
+  editHandler({ sender, rowIndex, dataItem }: any) {
+    this.closeEditor(sender);
+
+    this.formGroup = this.formBuilder.group({
+      ProductID: dataItem.ProductID,
+      ProductName: [dataItem.ProductName, [Validators.required]],
+      UnitPrice: dataItem.UnitPrice,
+      UnitsInStock: [
+        dataItem.UnitsInStock,
+        [Validators.required, Validators.pattern('^[0-9]{1,3}')],
+      ],
+      Discontinued: dataItem.Discontinued,
+    });
+
+    this.editedRowIndex = rowIndex;
+
+    sender.editRow(rowIndex, this.formGroup);
+  }
+
+  cancelHandler({ sender, rowIndex }: any) {
+    this.closeEditor(sender, rowIndex);
+  }
+
+  saveHandler({ sender, rowIndex, formGroup }: any) {
+    const product: Product = formGroup.value;
+    this.products = this.products.map((p) =>
+      p.ProductID === product.ProductID ? { ...p, ...product } : p
+    );
+    sender.closeRow(rowIndex);
+  }
+
+  private closeEditor(grid: any, rowIndex = this.editedRowIndex) {
+    grid.closeRow(rowIndex);
+    this.editedRowIndex = undefined;
+    this.formGroup = undefined;
   }
 }
