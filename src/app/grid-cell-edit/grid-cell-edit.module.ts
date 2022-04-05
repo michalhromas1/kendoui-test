cc5b09f9118a7ae9ec849bf715eabae121dc3e17
Michal Hromas <michalhromas1@gmail.com>
2022-04-05 13:35:05 +0200

    buttons

--- src/app/grid-cell-edit/grid-cell-edit.module.ts
+++ src/app/grid-cell-edit/grid-cell-edit.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { GridCellEditComponent } from './grid-cell-edit.component';
 
 @NgModule({
   declarations: [GridCellEditComponent],
-  imports: [CommonModule, ReactiveFormsModule, GridModule],
+  imports: [CommonModule, ReactiveFormsModule, GridModule, ButtonsModule],
   exports: [GridCellEditComponent],
 })
 export class GridCellEditModule {}
